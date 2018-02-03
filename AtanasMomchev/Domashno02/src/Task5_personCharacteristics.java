54695caebe596608a5e30cbf825f319d5bb1178d
Nasko <a.momchev1@gmail.com>
2018-02-03 10:19:48 +0200

    Auto stash before merge of "master" and "origin/master"
    Domashno 02 nasko

--- AtanasMomchev/Domashno02/src/Task5_personCharacteristics.java
+++ AtanasMomchev/Domashno02/src/Task5_personCharacteristics.java
@@ -5,14 +5,16 @@ public class Task5_personCharacteristics {
         Scanner input = new Scanner(System.in);
         System.out.println("How many characteristic's will you make? -");
         int number = input.nextInt();
+        //checking for input data
         while (true){
             if (number<0){
                 System.err.print("Must input positive number. Input now: ");
                 number =input.nextInt();
             }else break;
         }
+        //creating a cycle for the number of characteristics you want
         while (number > 0) {
-            System.out.println("Imputs for character number" + number);
+            //different inputs
             System.out.print("Imput first name:");
             String firstName = input.next();
             System.out.print("Imput last name:");
@@ -21,6 +23,7 @@ public class Task5_personCharacteristics {
             int birthYear = input.nextInt();
             System.out.print("Weight? - ");
             double weight = input.nextDouble();
+            //checking for correct input
             while (true){
                 if (weight<0){
                     System.err.print("Must input positive number. Input now: ");
@@ -29,6 +32,7 @@ public class Task5_personCharacteristics {
             }
             System.out.print("Height in cm pls? - ");
             int height = input.nextInt();
+            //checking for correct input
             while (true){
                 if (height<0){
                     System.err.print("Must input positive number. Input now: ");
@@ -38,10 +42,13 @@ public class Task5_personCharacteristics {
             System.out.print("What is her/his proffesion: ");
             input.nextLine();
             String job = input.nextLine();
+            //creating int for the current yeaer
             int currentYera = 2018;
+            //calculating how old is the person
             int age = currentYera - birthYear;
             System.out.printf("%s %s is %d years old.He was born in %d. His weight is %.2f and he is %d cm tall." +
                     "%nHe is a %s", firstName, lastName, age,birthYear, weight, height, job);
+            //funny if statement for age
             if (age < 10) {
                 System.out.format("%n %s %s is almost a baby O_O !!!", firstName, lastName);
             } else if (age < 18) {
@@ -52,6 +59,7 @@ public class Task5_personCharacteristics {
                 System.out.format("%n%s %s is a bit old", firstName, lastName);
             } else
                 System.out.format("%n%s %s is adult", firstName, lastName);
+            //reducing number to go to next iteration
             number--;
         }
         }
