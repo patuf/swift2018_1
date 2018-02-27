9cb399abbfa8627b7566d450ed0d2c77c43b6464
Unknown <georgi.p.ivanov21@abv.bg>
2018-02-27 12:06:38 +0200

    Georgi pause work

---
+++ georgi_spas/src/PersonCharacteristics.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class PersonCharacteristics {
+
+    public static void main(String[] args) {
+
+        // input whit scanner
+        Scanner sc=new Scanner(System.in);
+
+
+        String fName=sc.nextLine();
+        while (true){
+           // for (int i = 0; i <fName.length() ; i++) {
+             //   if (fName.)
+           // }
+        }
+
+      //  String surname;
+        //int yearsBorn;
+        //double weight;
+        //double height;
+        //String profession;
+
+
+
+        //check input data and iskame povtornpo vuvjevda ne pri greshka
+
+
+        // check liceto e nad 18
+
+        // <name> <last name>is< godini>.
+
+    }
+
+}
