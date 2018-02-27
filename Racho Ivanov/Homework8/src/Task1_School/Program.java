48cf970c24392624d42333f6c45845f2118b3086
Unknown <ra4id@abv.bg>
2018-02-27 15:38:26 +0200

    HomeWork8

---
+++ Racho Ivanov/Homework8/src/Task1_School/Program.java
@@ -0,0 +1,10 @@
+package Task1_School;
+
+public class Program
+{
+	//Вероятно енумът не е най-правилния клас но исках да го пробвам в програма.
+	public static void main(String[] args)
+	{
+
+	}
+}
