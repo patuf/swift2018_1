2e689db329d25ae5b8ff4cf02abee4cf38d260ac
Unknown <georgi.p.ivanov21@abv.bg>
2018-02-27 14:53:09 +0200

    georgi-> hmw08-task school hierarchy

---
+++ georgi_spas/src/hmw08/school/Discipline.java
@@ -0,0 +1,14 @@
+package hmw08.school;
+
+public class Discipline {
+
+    String disciplineName;
+    double hoursPerWeek;
+
+
+    Discipline(String disciplineName,double hoursPerWeek){
+        this.disciplineName=disciplineName;
+        this.hoursPerWeek=hoursPerWeek;
+    }
+
+}
