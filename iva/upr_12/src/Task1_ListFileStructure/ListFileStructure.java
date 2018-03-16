01a57d9726b1f55f00f332bdedaee6058f706246
Iva <ivetosofiq@gmail.com>
2018-03-16 17:57:19 +0200

    Iva update Homework 12: Task1

--- iva/upr_12/src/Task1_ListFileStructure/ListFileStructure.java
+++ iva/upr_12/src/Task1_ListFileStructure/ListFileStructure.java
@@ -1,20 +1,32 @@
 package Task1_ListFileStructure;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ListFileStructure {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Path printDir = Paths.get("F:\\SwiftAcademy\\Lectures and HW");
 
         File testDir = new File(String.valueOf(printDir));
         File[] files = testDir.listFiles();
 
         assert files != null;
-        for (File file : files) {
-            System.out.println(file.getAbsolutePath());
-        }
+        listFolder(files);
+    }
 
+    private static void listFolder(File[] files){
+        for (File f : files){
+            if (f.isDirectory()){
+                File testFolder = new File(f.getPath());
+                File [] folder = testFolder.listFiles();
+                assert folder != null;
+                for (File file : folder){
+                    System.out.println(file.getAbsolutePath());
+                }
+            } else
+                System.out.println(f.getAbsolutePath());
+        }
     }
 }
